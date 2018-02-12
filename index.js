ab10956152fe408f134e2af48ecd47a7d07482d4
ripleyofvenus <ripleyofvenus@gmail.com>
2018-02-12 14:58:13 -0500

    Take Photo Button connected to takePhoto()
    Button calls takePhoto and a small image appears in the strip at the
    bottom underneath the canvas.
    If the user wishes to download one of their photos, they simply click
    the image they wish to keep.

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function takePhoto() {
   const link = document.createElement('a')
   link.href = data
   link.setAttribute('download', 'WorkTheCamera')
-  link.textContent = 'Download Image'
+  link.innerHTML = `<img src="${data}" alt="Pose"/>`
   strip.insertBefore(link, strip.firstChild)
 }
 
